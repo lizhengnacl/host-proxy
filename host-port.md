3ee6689ef10683771eb48336839a3d1fada53d92
lizheng11 <lizheng11@meituan.com>
2016-12-30 17:56:14 +0800

    init

---
+++ host-port.md
@@ -0,0 +1,2 @@
+127.0.0.1 oa.dev.neixin.cn 9002
+127.0.0.1 web.xm.test.sankuai.com 9800
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liz
+ * Email: lizhengnacl@163.com
+ * Tel: 18686768624
+ * Date: 16/12/29
+ */
+
+let proxy = require('./lib/proxy');
+
+proxy(80, './host-port.md');
